e03efb3d38a4c3737bf255fafb8becd6ac52257a
Lorenzo <lorenzoburgi@gmail.com>
2021-07-11 13:51:27 -0300

    terminado

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const queryUl = document.querySelectorAll(".lista li");
+  const listaEl = document.querySelector(".lista");
+  queryUl.forEach((element) => {
+    element.remove();
+  });
+
+  cosasQueAprendimos.forEach((element) => {
+    const newList = document.createElement("li");
+    newList.textContent = element.tema;
+    newList.classList.add(element.class || "item");
+    console.log(newList);
+    listaEl.appendChild(newList);
+  });
+}
 
 main();
